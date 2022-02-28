1ae6c912ef95bee59d2879b88f2909c0657d2437
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-02-28 16:20:48 +0200

    Pelaajalista

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,34 @@
+import requests
+from datetime import datetime
 from player import Player
 
 def main():
-    pass
+    nationality = 'FIN'
+
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    print(f"Players from {nationality} {datetime.now()}")
+
+    players = []
+
+    for player_dict in response:
+        if player_dict['nationality'] != nationality:
+            continue
+
+        player = Player(
+            player_dict['name'],
+            player_dict['team'],
+            player_dict['goals'],
+            player_dict['assists']
+        )
+
+        players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
