f241f436355c39c55802aada40bbfa4383ede2e6
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-08 15:46:28 +0200

    Parannettu kyselykieli osa 2

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -2,16 +2,19 @@ from matchers import And, Or, Not, All, PlaysIn, HasAtLeast, HasFewerThan
 
 class QueryBuilder:
     def __init__(self, query = All()):
-        self.query_object = query
+        self._query_object = query
+
+    def oneOf(self, *queries):
+        return QueryBuilder(And(Or(*queries), self._query_object))
 
     def playsIn(self, team):
-        return QueryBuilder(And(PlaysIn(team), self.query_object))
+        return QueryBuilder(And(PlaysIn(team), self._query_object))
 
     def hasAtLeast(self, value, attr):
-        return QueryBuilder(And(HasAtLeast(value, attr), self.query_object))
+        return QueryBuilder(And(HasAtLeast(value, attr), self._query_object))
 
     def hasFewerThan(self, value, attr):
-        return QueryBuilder(And(HasFewerThan(value, attr), self.query_object))
+        return QueryBuilder(And(HasFewerThan(value, attr), self._query_object))
 
     def build(self):
-        return self.query_object
+        return self._query_object
