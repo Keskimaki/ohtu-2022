f384f9a677beb2c2f3fa48130228ade6ee1e4492
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-08 16:34:51 +0200

    Laskin- ja komento-oliot

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -13,3 +13,32 @@ class Sovelluslogiikka:
 
     def aseta_arvo(self, arvo):
         self.tulos = arvo
+
+class Summa:
+    def __init__(self, sovellus, lue_syote):
+        self._sovellus = sovellus
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        arvo = int(self._lue_syote())
+        self._sovellus.plus(arvo)
+
+class Erotus:
+    def __init__(self, sovellus, lue_syote):
+        self._sovellus = sovellus
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        arvo = int(self._lue_syote())
+        self._sovellus.miinus(arvo)
+
+class Nollaus:
+    def __init__(self, sovellus):
+        self._sovellus = sovellus
+
+    def suorita(self):
+        self._sovellus.nollaa()
+
+class Kumoa:
+    def __init__(self, sovellus, lue_syote):
+        pass
